5cd8aa4f24e9ca87dc9852d9b7bc3de8f5fd1848
macpro <kida@cysec.cs.ritsumei.ac.jp>
2025-06-17 18:00:46 +0900

    fix: port

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,10 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  plugins: [react()]
-});
+  plugins: [react()],
+  preview: {
+    port: process.env.PORT ? parseInt(process.env.PORT) : 5173,
+    host: true
+  }
+})
